87767d51c22303149bdc57cbd30b33d6ba96a590
KimDohyoun79 <dogdo5726@gmail.com>
2022-12-19 11:24:55 +0900

    [First] commit

---
+++ src/main/java/com/ll/exam/app_2022_12_19_1_chat/App202212191ChatApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_2022_12_19_1_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App202212191ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App202212191ChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/exam/app_2022_12_19_1_chat/App202212191ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.app_2022_12_19_1_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class App202212191ChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
